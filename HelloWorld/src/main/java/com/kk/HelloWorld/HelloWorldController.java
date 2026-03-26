748fbc33ecf7855afbf1a9eb7503743b86854f63
KARTHIKAKRISHNA123 <athvikaalm2018@gmail.com>
2026-03-26 08:22:43 +0530

    feat: Created first API with SpringBoot

---
+++ HelloWorld/src/main/java/com/kk/HelloWorld/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.kk.HelloWorld;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello")
+
+    String sayHelloWorld() {
+        return "Hello World!";
+    }
+}
